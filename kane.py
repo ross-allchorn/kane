f9fea0215768000e90ea640ff34b23c259d32bb2
Ross Allchorn <41346206+ross-allchorn@users.noreply.github.com>
2024-07-26 20:59:36 +0200

    Added basic python script
    Still to be tested and debugged.

--- kane.py
+++ kane.py
@@ -1,3 +1,13 @@
-# Kane is a pro
-# Copyleft 2024
-# Because I can
+import string
+import random
+import datetime
+
+def random_alphanumeric(length):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for _ in range(length))
+
+filename = random_alphanumeric(5) + ".txt"
+message = f"This random named file was created at precisely {datetime.datetime.now():%Y/%m/%d %H:%M:%S}"
+
+with open(filename, "w") as f:
+    f.write(message)
